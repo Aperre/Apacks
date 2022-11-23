14a40a6fd6bd7550158db54964f13698debba4dd
Aperre <60411288+Aperre@users.noreply.github.com>
2022-11-23 22:10:19 +0100

    First Release

---
+++ src/main/java/net/apacks/mixin/PlayerMove.java
@@ -0,0 +1,24 @@
+package net.apacks.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+
+@Mixin(PlayerMoveC2SPacket.class)
+public class PlayerMove {
+    private MinecraftClient mc;
+    @ModifyArgs(method="Full",at=@At(value = "INVOKE"))
+    private void mixin(Args args) {
+        args.set(0, Math.floor(mc.player.getX()*10)/10);
+        args.set(2, Math.floor(mc.player.getZ()*10)/10);
+    }
+
+
+
+    // PositionAndOnGround
+
+}
