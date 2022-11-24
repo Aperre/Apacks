d42a88da9af789274f67847decbe5303f93990c1
Aperre <60411288+Aperre@users.noreply.github.com>
2022-11-24 16:10:50 +0100

    Position OvFl Test 1

--- src/main/java/net/apacks/mixin/PlayerMove.java
+++ src/main/java/net/apacks/mixin/PlayerMove.java
@@ -1,25 +1,30 @@
 package net.apacks.mixin;
 
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
-import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 
-@Mixin(PlayerMoveC2SPacket.Full.class)
-public class PlayerMove {
-    private MinecraftClient mc;
-    @ModifyArgs(method="Full",at=@At(value = "INVOKE"))
-    private void mixin(@NotNull Args args) {
-        args.set(0, Math.floor(mc.player.getX()*10)/10);
-        args.set(2, Math.floor(mc.player.getZ()*10)/10);
+@Mixin(PlayerMoveC2SPacket.PositionAndOnGround.class)
+class PosAndGround {
+    @ModifyVariable(method="Lnet/minecraft/network/packet/c2s/play/PlayerMoveC2SPacket$PositionAndOnGround;<init>(DDDZ)V",at = @At("HEAD"),ordinal = 1)
+    private double injectedx(double x) {
+        return Math.floor(x*100)/100;
     }
-
-
-
-    // PositionAndOnGround
-
+    @ModifyVariable(method="Lnet/minecraft/network/packet/c2s/play/PlayerMoveC2SPacket$PositionAndOnGround;<init>(DDDZ)V",at = @At("HEAD"),ordinal = 1)
+    private double injectedz(double z) {
+        return Math.floor(z*100)/100;
+    }
+}
+@Mixin(PlayerMoveC2SPacket.Full.class)
+class PlayerMove {
+    @ModifyVariable(method="Lnet/minecraft/network/packet/c2s/play/PlayerMoveC2SPacket$Full;<init>(DDDFFZ)V",at = @At("HEAD"),ordinal = 1)
+    private double injectedx(double x) {
+        return Math.floor(x*100)/100;
+    }
+    @ModifyVariable(method="Lnet/minecraft/network/packet/c2s/play/PlayerMoveC2SPacket$Full;<init>(DDDFFZ)V",at = @At("HEAD"),ordinal = 1)
+    private double injectedz(double z) {
+        return Math.floor(z*100)/100;
+    }
 }
