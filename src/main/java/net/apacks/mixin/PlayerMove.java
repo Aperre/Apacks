ff369811a89e88038ceff122c3a3f11fa618da75
Aperre <60411288+Aperre@users.noreply.github.com>
2022-11-27 21:33:36 +0100

    Position OvFl Test 2 final fix

--- src/main/java/net/apacks/mixin/PlayerMove.java
+++ src/main/java/net/apacks/mixin/PlayerMove.java
@@ -5,26 +5,25 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
 
-@Mixin(PlayerMoveC2SPacket.PositionAndOnGround.class)
-class PosAndGround {
-    @ModifyVariable(method="Lnet/minecraft/network/packet/c2s/play/PlayerMoveC2SPacket$PositionAndOnGround;<init>(DDDZ)V",at = @At("HEAD"),ordinal = 1)
-    private double injectedx(double x) {
-        return Math.floor(x*100)/100;
-    }
-    @ModifyVariable(method="Lnet/minecraft/network/packet/c2s/play/PlayerMoveC2SPacket$PositionAndOnGround;<init>(DDDZ)V",at = @At("HEAD"),ordinal = 1)
-    private double injectedz(double z) {
-        return Math.floor(z*100)/100;
-    }
-}
-@Mixin(PlayerMoveC2SPacket.Full.class)
+
+@Mixin(PlayerMoveC2SPacket.class)
 class PlayerMove {
-    @ModifyVariable(method="Lnet/minecraft/network/packet/c2s/play/PlayerMoveC2SPacket$Full;<init>(DDDFFZ)V",at = @At("HEAD"),ordinal = 1)
-    private double injectedx(double x) {
-        return Math.floor(x*100)/100;
+
+    @ModifyVariable(method="Lnet/minecraft/network/packet/c2s/play/PlayerMoveC2SPacket;<init>(DDDFFZZZ)V",at = @At("HEAD"),ordinal = 0)
+    private static double injectedx(double x) {
+        BigDecimal newx = BigDecimal.valueOf(x);
+        double newdx = newx.round(new MathContext(3)).doubleValue();
+        return newdx;
     }
-    @ModifyVariable(method="Lnet/minecraft/network/packet/c2s/play/PlayerMoveC2SPacket$Full;<init>(DDDFFZ)V",at = @At("HEAD"),ordinal = 1)
-    private double injectedz(double z) {
-        return Math.floor(z*100)/100;
+
+    @ModifyVariable(method="Lnet/minecraft/network/packet/c2s/play/PlayerMoveC2SPacket;<init>(DDDFFZZZ)V",at = @At("HEAD"),ordinal = 2)
+    private static double injectedz(double z) {
+        BigDecimal newz = BigDecimal.valueOf(z);
+        double newdz = newz.round(new MathContext(3)).doubleValue();
+        return newdz;
     }
 }
+
