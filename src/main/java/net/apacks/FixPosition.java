8bfa9d0097c5d56b016760861e847d2e15d680ae
Aperre <loleur12345@gmail.com>
2022-12-01 12:28:55 +0100

    Optimisation

--- src/main/java/net/apacks/FixPosition.java
+++ src/main/java/net/apacks/FixPosition.java
@@ -3,14 +3,16 @@ package net.apacks;
 import net.minecraft.client.MinecraftClient;
 
 public class FixPosition {
+    static MinecraftClient client = MinecraftClient.getInstance();
     public static double FixPositionX() {
-        return (EasyRounding.round(MinecraftClient.getInstance().player.getPos().getX()*100))/100;
+        
+        return (EasyRounding.round(client.player.getPos().getX()*100))/100;
     }
     public static double FixPositionZ() {
-        return (EasyRounding.round(MinecraftClient.getInstance().player.getPos().getX()*100))/100;
+        return (EasyRounding.round(client.player.getPos().getX()*100))/100;
     }
 
     public static void Fix(){
-        PacketHelper.sendExactPosition(FixPositionX(),MinecraftClient.getInstance().player.getPos().getY(),FixPositionZ());
+        PacketHelper.sendExactPosition(FixPositionX(),client.player.getPos().getY(),FixPositionZ());
     }
 }
