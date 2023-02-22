06c07a4187726dd48dc9ec65365b4a767380a3cf
ape <loleur12345@gmail.com>
2023-02-22 23:28:10 +0100

    Fixed Random ass bugs notably idfk

--- src/main/java/net/apacks/Main.java
+++ src/main/java/net/apacks/Main.java
@@ -9,6 +9,7 @@ import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallba
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.command.argument.PosArgument;
 import net.minecraft.command.argument.Vec3ArgumentType;
+import net.minecraft.text.Text;
 import net.minecraft.util.math.Vec3d;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,7 +29,7 @@ public class Main implements ModInitializer {
         LOGGER.info("Initialized Apacks");
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
             // Register the "teleport" command
-            dispatcher.register(ClientCommandManager.literal("teleport").then(
+            dispatcher.register(ClientCommandManager.literal("ctp").then(
                     ClientCommandManager.argument("location", Vec3ArgumentType.vec3()).executes(
                             context -> {
 
@@ -71,31 +72,43 @@ public class Main implements ModInitializer {
                             })
                     )
             );
-            dispatcher.register(ClientCommandManager.literal("auth")
-                    .then(ClientCommandManager.argument("token", StringArgumentType.string())
-                            .executes(context -> {
-
-                                String token = StringArgumentType.getString(context, "token");
-
-
-                                client.inGameHud.getChatHud().addToMessageHistory(Objects.requireNonNull(client.getSession().getSessionId()));
-                                return 1;
-                            })
-                    )
+            dispatcher.register(ClientCommandManager.literal("getsessionid")
+                    .executes(context -> {
+                        String session = Objects.requireNonNull(client.getSession().getSessionId());
+                        context.getSource().sendFeedback(Text.of(session));
+                        client.inGameHud.getChatHud().addToMessageHistory(session);
+                        return 1;
+                    })
             );
-            JShell js = JShell.create();
-            dispatcher.register(ClientCommandManager.literal("eval")
-                    .then(ClientCommandManager.argument("code", StringArgumentType.string())
-                            .executes(context -> {
 
-                                String code = StringArgumentType.getString(context, "code");
+            //TODO: ADD AUTH COMMAND
 
-                                js.eval(code);
+            try {
+                JShell js = JShell.create();
+                dispatcher.register(ClientCommandManager.literal("eval")
+                        .then(ClientCommandManager.argument("code", StringArgumentType.string())
+                                .executes(context -> {
 
-                                return 1;
-                            })
-                    )
-            );
+                                    String code = StringArgumentType.getString(context, "code");
+
+                                    js.eval(code);
+
+                                    return 1;
+                                })
+                        )
+                );
+            } catch (Exception e) {
+                dispatcher.register(ClientCommandManager.literal("eval")
+                        .then(ClientCommandManager.argument("code", StringArgumentType.string())
+                                .executes(context -> {
+
+                                    context.getSource().sendError(Text.of("JShell Couldn't get initialized"));
+
+                                    return 1;
+                                })
+                        )
+                );
+            }
 
 
                 }
